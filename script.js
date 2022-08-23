f30c83c17b635da9b0d14603047a42e3c8ec7617
Jakub Musialowski <mds@mds.biz.pl>
2022-08-23 02:26:40 +0200

    Responsive style improved, Deleted unnecessary code

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-
-// let kursEuro = document.querySelector(".kursy__euro").innerText;
-// let kursDolar = document.querySelector(".kursy__dolar").innerText;
-// let kursFrank = document.querySelector(".kursy__frank").innerText;
-// let buttonReset = document.querySelector(".buttonReset");
-// let wybranaWalutaKurs = document.querySelector(".wybrana_waluta--kurs");
-// let pole3 = Number(kursEuro);
-// let pole4 = +kursEuro;
-// Sprawdzenie zawartości pola input przez wartośc value
-// convertedAmount.value
-// let wybranaWaluta = document.querySelector(".wybrana_waluta");
 let exchangeForm = document.querySelector(".js-form");
-
 
 let kurs_eur = 4.7480;
 let kurs_dol = 4.7061;
@@ -25,98 +13,31 @@ let selectedCurrency = document.querySelector(".js-selectedCurrency");
 let selectedCurrencyPrice = document.querySelector(".js-selectedCurrencyPrice");
 let selectedCurrencyRate = document.querySelector(".js-selectedCurrencyRate");
 
-// wybranaWalutaKurs.innerText = (1 / kurs_eur).toFixed(3);
 selectedCurrencyPrice.innerText = (1 / kurs_eur).toFixed(3);
 selectedCurrency.innerText = "EUR";
 selectedCurrencyRate.innerText = kurs_eur;
-// formularz.addEventListener("input", () =>{
-//     if (euro.checked === true){
-//         pole2.value = kursEuro;
-//         }
-//         else if (dolar.checked === true){
-//             pole2.value = kursDolar;
-//         } else if (frank.checked === true){
-//             pole2.value = kursFrank;
-//         }
 
-
-//     let przeliczenie = +pole.value * +pole2.value;
-
-//    wynik.innerText = przeliczenie;
-// })
-
-// let listakursy = document.querySelector(".lista-kursy").value;
-// let listawybranawakuta = document.querySelector(".lista-kursy-wybrana");
-
-// switch (listakursy){
-// case "euro":
-//     listawybranawakuta.innerText="Euro";
-// brake;
-// case "dolar":
-//     listawybranawakuta.innerText = "Dolar";
-//     brake;
-// case "frank":
-//     listawybranawakuta.innerText = "Frank";
-//     break;
-// }
 let currencyConverter;
 exchangeForm.addEventListener("input", () => {
     
-
     if (euroCurrency.checked === true) {
-        // pole2.value = kursEuro;  //zdefinować zmienną z kursem waluty
-        // wybranaWaluta.innerText = "EUR";
         selectedCurrency.innerText = "EUR";
         selectedCurrencyPrice.innerText = (1 / kurs_eur).toFixed(3);
         selectedCurrencyRate.innerText = kurs_eur; 
         currencyConverter = "eur";
     }
     else if (dolarCurrency.checked === true) {
-        // pole2.value = kursDolar; //zdefinować zmienną z kursem waluty
-        // wybranaWaluta.innerText = "DOL";
         selectedCurrency.innerText = "DOL";
         selectedCurrencyPrice.innerText = (1 / kurs_dol).toFixed(3);
         selectedCurrencyRate.innerText = kurs_dol; 
         currencyConverter = "dol";
     } else if (frankCurrency.checked === true) {
-        // pole2.value = kursFrank; //zdefinować zmienną z kursem waluty
-        // wybranaWaluta.innerText = "CHF";
         selectedCurrency.innerText = "CHF";
         selectedCurrencyPrice.innerText = (1 / kurs_chf).toFixed(3);
         selectedCurrencyRate.innerText = kurs_chf;
         currencyConverter = "chf";
     }
 })
-
-// formularz.addEventListener("input", () =>{
-//     if (euro.checked === true){
-//         pole2.value = kursEuro;
-//         wybranaWaluta.innerText = "euro";
-//         }
-//         else if (dolar.checked === true){
-//             pole2.value = kursDolar;
-//             wybranaWaluta.innerText = "dolar";
-//         } else if (frank.checked === true){
-//             pole2.value = kursFrank;
-//             wybranaWaluta.innerText = "frank";
-//         }
-
-
-// let listakursy = document.querySelector(".lista-kursy").value;
-// let listawybranawakuta = document.querySelector(".lista-kursy-wybrana");
-
-// switch (listakursy){
-// case "euro":
-//     listawybranawakuta.innerText="Euro";
-// brake;
-// case "dolar":
-//     listawybranawakuta.innerText = "Dolar";
-//     brake;
-// case "frank":
-//     listawybranawakuta.innerText = "Frank";
-//     break;
-// }
-// })
 
 exchangeForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -136,14 +57,6 @@ exchangeForm.addEventListener("submit", (event) => {
         break;
     }
 
-
-
-    // let przeliczenie = +pole.value * +pole2.value;
     formSummaryConvertedAmount.innerText = `${convertedAmount.value} PLN = `;
     formResult.innerText = `${przeliczenie.toFixed(2)} ${selectedCurrency.innerText}`;
-
 })
-
-//  buttonReset.addEventListener("click", () => {
-//      wynik.innerText = "";
-//  })
